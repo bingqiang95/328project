c00d9ce97cfc019fe07a6f6126946e623d3382e1
何小明 <1490045329@qq.com>
2018-12-27 18:00:43 +0800

    测试Git2

--- pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/GitTest.java
+++ pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/GitTest.java
@@ -2,6 +2,8 @@ package cn.itcast.core.service;
 
 public class GitTest {
     public static void main(String[] args) {
+        String name="Test Hm328";
+        System.out.println(name);
         System.out.println("Hello Git");
     }
 }
